ce208ef5d53ee34877beda4576ac7e2100eeadcc
Waller Goble <waller.go@gmail.com>
2016-11-20 20:02:16 -0500

    Added pop up randomizer to start and on hit, added multiple moles.

--- app.js
+++ app.js
@@ -2,15 +2,24 @@
 
 let scoreCounter = 0;
 
-$('.mole').on('click', hit);
+
+// Helper function to remove hide class on moles.
+function randomUnhide(obj, min, max) {
+  setTimeout(function() {
+    $(obj).removeClass('hide');
+  }, getRandomInt(min, max));
+}
 
 function hit() { // not very modular
   scoreCounter++;
   updateScore();
   $(this).addClass('hide');
   let that = this;
-  setTimeout(function() { $(that).removeClass('hide') }, 1000);
+  randomUnhide(that, 3000, 7000);
 }
+
+$('.mole').on('click', hit);
+
 
 function updateScore() {        // need to make this not highlightable.
   console.log("goal")
@@ -39,10 +48,25 @@ function startTimer() {
 //      ### Start Game ###
 function startGame() {
     console.log('Hello from startGame')
-    seconds = 6;
+    seconds = 30;
     scoreCounter = 0;
     startTimer();
-    $('.mole').removeClass('hide')
+    randomUnhide('#js-mole-1', 2000, 2000);
+    randomUnhide('#js-mole-2', 3000, 5000);
+    randomUnhide('#js-mole-3', 3000, 5000);
+    randomUnhide('#js-mole-4', 3000, 5000);
+    randomUnhide('#js-mole-5', 3000, 5000);
+    randomUnhide('#js-mole-6', 3000, 5000);
+    // if (seconds == 0) {
+    //   return stopGame();
+    // // setTimeout(function () { stopGame}, (seconds * 1000))
 }
 
+// Make an end game function
+
 // Function that generates a number between 1500 and 3000
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
