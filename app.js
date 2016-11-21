c8c8ee5582159044475136e0d2a7c718ad6196c2
Waller Goble <waller.go@gmail.com>
2016-11-21 15:29:44 -0500

    fRefactored pop up randomization logic

--- app.js
+++ app.js
@@ -1,72 +1,96 @@
 //      ### Scoring Functions ###
 
-let scoreCounter = 0;
-
+var scoreCounter = 0;
+var $moles = $('.mole');
+var molesArray = $moles.toArray();       // Necessary to use forEach to iterate through moles.
+let $mole1 = $("#js-mole-1");
 
 // Helper function to remove hide class on moles.
 function randomUnhide(obj, min, max) {
-  setTimeout(function() {
-    $(obj).removeClass('hide');
-  }, getRandomInt(min, max));
+    setTimeout(function() {
+        $(obj).removeClass('hide');
+    }, getRandomInt(min, max));
+}
+
+function randomHide(moles) {
+    moles.forEach(function(mole) {
+      console.log(mole.className.includes("mole"));
+        if (mole.className.includes("hide")) {
+            if (Math.random() <= 0.2) {
+                console.log('show');
+                mole.classList.remove("hide");
+            }
+        } else {
+            if (Math.random() <= 0.6) {
+                console.log('hide');
+                mole.classList.add("hide");
+            }
+        }
+    })
 }
 
 function hit() { // not very modular
-  scoreCounter++;
-  updateScore();
-  $(this).addClass('hide');
-  let that = this;
-  randomUnhide(that, 3000, 7000);
+    scoreCounter++;
+    updateScore();
+    $(this).addClass('hide');
+    // let that = this;
+    // randomUnhide(that, 3000, 7000);
 }
 
 $('.mole').on('click', hit);
 
 
-function updateScore() {        // need to make this not highlightable.
-  console.log("goal")
-  $('.score-span').html(scoreCounter);
+function updateScore() { // need to make this not highlightable.
+    console.log("goal")
+    $('.score-span').html(scoreCounter);
 }
 
 
 //      ### Timer Functions ###
 
-let seconds = 60;               // Starting place for the timer
-let myTimer = null;             // This is necessary to clear the interval
+let seconds = 5; // Starting place for the timer
+let myTimer = null; // This is necessary to clear the interval
 
 // This function increments the timer and dynamically updates the DOM
-function showTimer(){
-  if (seconds <= 0){
-    clearInterval(myTimer);
-  }
-  $('.timer-span').html(seconds)
-  seconds--;
+function showTimer() {
+    if (seconds <= 0) {
+        clearInterval(myTimer);
+    }
+    $('.timer-span').html(seconds)
+    seconds--;
 };
 
 function startTimer() {
-  myTimer = window.setInterval(showTimer,1000);
+    myTimer = window.setInterval(showTimer, 1000);
 }
 
 //      ### Start Game ###
 function startGame() {
     console.log('Hello from startGame')
-    seconds = 30;
     scoreCounter = 0;
     startTimer();
-    randomUnhide('#js-mole-1', 2000, 2000);
-    randomUnhide('#js-mole-2', 3000, 5000);
-    randomUnhide('#js-mole-3', 3000, 5000);
-    randomUnhide('#js-mole-4', 3000, 5000);
-    randomUnhide('#js-mole-5', 3000, 5000);
-    randomUnhide('#js-mole-6', 3000, 5000);
-    // if (seconds == 0) {
-    //   return stopGame();
-    // // setTimeout(function () { stopGame}, (seconds * 1000))
+    setInterval(function() {randomHide(molesArray);}, 1000)
+    
+    // randomUnhide('#js-mole-1', 2000, 2000);
+    // randomUnhide('#js-mole-2', 3000, 5000);
+    // randomUnhide('#js-mole-3', 3000, 5000);
+    // randomUnhide('#js-mole-4', 3000, 5000);
+    // randomUnhide('#js-mole-5', 3000, 5000);
+    // randomUnhide('#js-mole-6', 3000, 5000);
+    // endGame();
 }
 
 // Make an end game function
 
-// Function that generates a number between 1500 and 3000
+function endGame() {
+    setTimeout(function() {
+        $('div.mole').addClass('hide')
+    }, ((seconds + 2) * 1000))
+}
+
+// Function that generates a number between a range
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
